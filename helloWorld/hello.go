305036a08efe7ad65f32917b6a53c7037c31f686
Farrukh Askari <farrukh.askari01@gmail.com>
2017-05-19 22:37:14 +0500

    Initial commit

---
+++ helloWorld/hello.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Printf("helo world\n")
+	handleRequests()
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+// func main() {
+//     handleRequests()
+// }
